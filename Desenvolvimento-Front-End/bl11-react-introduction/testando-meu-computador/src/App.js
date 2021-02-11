0bc27efb1c59424beff4424ae27ccf9ae4e6489b
Genadi Reuben <genadireuben91@gmail.com>
2021-02-11 18:05:09 -0300

    novo bloco de front-end

---
+++ Desenvolvimento-Front-End/bl11-react-introduction/testando-meu-computador/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import FirstReact from './Component.js';
+
+function App() {
+  return (
+    <FirstReact />
+  );
+
+};
+
+export default App;
---
+++ Desenvolvimento-Front-End/bl11-react-introduction/testando-meu-computador/src/Component.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class FirstReact extends React.Component {
+    render() {
+        return (<div>
+            <h1> Genadi</h1>
+            <p> Forte e determinado</p>
+        </div>
+        )
+    }
+
+}
+
+export default FirstReact;
